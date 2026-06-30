60b1cc72f2c3868dc8e94a7b58b1e53884d440a4
mrjuarez889-a11y <mr.juarez889@gmail.com>
2026-06-30 18:58:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+let total = 0;
 
+const menu = document.getElementById("menu");
+const totalTexto = document.getElementById("total");
+
+bebidas.forEach((bebida)=>{
+
+const tarjeta = document.createElement("div");
+tarjeta.className="drink-card";
+
+tarjeta.innerHTML=`
+<img src="${bebida.imagen}">
+<h2>${bebida.nombre}</h2>
+<p>$${bebida.precio.toFixed(2)}</p>
+
+<button>➕ Agregar</button>
+`;
+
+const boton=tarjeta.querySelector("button");
+
+boton.onclick=()=>{
+
+total+=bebida.precio;
+
+totalTexto.textContent="$"+total.toFixed(2);
+
+};
+
+menu.appendChild(tarjeta);
+
+});
