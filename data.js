932cd21f0d82b584ef95cdecccd92b6904f8bb2e
mrjuarez889-a11y <mr.juarez889@gmail.com>
2026-07-01 09:55:15 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,26 @@
 const bebidas = [
   {
     nombre: "Freedom Splash",
+    descripcion: "Sprite + Blue Raspberry + Sweet Cream",
     precio: 6.32,
-    imagen: "https://picsum.photos/400/300?random=1"
+    imagen: "https://picsum.photos/600/400?1"
   },
   {
     nombre: "Sunset Paradise",
+    descripcion: "Dr Pepper + Coconut + Vanilla",
     precio: 6.32,
-    imagen: "https://picsum.photos/400/300?random=2"
+    imagen: "https://picsum.photos/600/400?2"
   },
   {
     nombre: "Pink Sunset",
+    descripcion: "Sprite + Strawberry + Cream",
     precio: 6.32,
-    imagen: "https://picsum.photos/400/300?random=3"
+    imagen: "https://picsum.photos/600/400?3"
   },
   {
     nombre: "Ocean Breeze",
+    descripcion: "Mountain Dew + Blue Coconut",
     precio: 6.32,
-    imagen: "https://picsum.photos/400/300?random=4"
+    imagen: "https://picsum.photos/600/400?4"
   }
 ];
